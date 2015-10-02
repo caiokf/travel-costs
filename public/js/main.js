fbe002ec39554a1aef9407041244808c97b3a06f
Caio Kinzel Filho <caiokf@gmail.com>
2015-10-02 12:21:01 +0200

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: {lat: 0, lng: 0},
+    zoom: 2
+  });
+
+  var transportationSlider = document.getElementById('slider-transportation');
+  noUiSlider.create(transportationSlider, {
+    start: [ 4000 ],
+    range: {
+      'min': [  2000 ],
+      'max': [ 10000 ]
+    }
+  });
+
+  var foodSlider = document.getElementById('slider-food');
+  noUiSlider.create(foodSlider, {
+    start: [ 4000 ],
+    range: {
+      'min': [  2000 ],
+      'max': [ 10000 ]
+    }
+  });
+
+  var accommodationSlider = document.getElementById('slider-accommodation');
+  noUiSlider.create(accommodationSlider, {
+    start: [ 4000 ],
+    range: {
+      'min': [  2000 ],
+      'max': [ 10000 ]
+    }
+  });
+
+});
