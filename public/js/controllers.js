f07b7a0e3d5181273cb7d0becb2a1f56e7c77bc3
Caio Kinzel Filho <caiokf@gmail.com>
2015-10-03 14:53:01 +0200

    calculate all costs based on sliders for one day, default location

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -40,7 +40,60 @@ angular.module('travelCosts.controllers', ['travelCosts.services']).
       waypoint.marker.setMap(null);
     };
 
+    $scope.$watch('sliderFood', function() { $scope.calculateCost(); });
+    $scope.$watch('sliderTransportation', function() { $scope.calculateCost(); });
+    $scope.$watch('sliderAccommodation', function() { $scope.calculateCost(); });
+
     $scope.calculateCost = function () {
       var costs = CostsService.getCostFor('location');
+
+      function multipliers(slider) {
+        var option1, option2, option3;
+
+        if (slider == 10) {
+          option1 = 0;
+          option2 = 0;
+          option3 = 1;
+        } else if (slider == 0) {
+          option1 = 1;
+          option2 = 0;
+          option3 = 0;
+        } else if (slider == 5) {
+          option1 = 0;
+          option2 = 1;
+          option3 = 0;
+        } else {
+          option1 = (5 - slider) > 0 ? ((5 - slider) * 0.2) : 0;
+          option3 = (slider - 5) > 0 ? ((slider - 5) * 0.2) : 0;
+
+          if (slider > 5) {
+            option2 = (10 - slider) * 0.2;
+          } else {
+            option2 = (slider) * 0.2;
+          }
+        }
+
+        return {option1: option1, option2: option2, option3: option3 };
+      };
+
+      var foodCost = 0;
+      var foodMultipliers = multipliers($scope.sliderFood);
+      foodCost += costs.food.option1 * foodMultipliers.option1;
+      foodCost += costs.food.option2 * foodMultipliers.option2;
+      foodCost += costs.food.option3 * foodMultipliers.option3;
+
+      var accommodationCost = 0;
+      var accommodationMultipliers = multipliers($scope.sliderAccommodation);
+      accommodationCost += costs.accommodation.option1 * accommodationMultipliers.option1;
+      accommodationCost += costs.accommodation.option2 * accommodationMultipliers.option2;
+      accommodationCost += costs.accommodation.option3 * accommodationMultipliers.option3;
+
+      var transportationCost = 0;
+      var transportationMultipliers = multipliers($scope.sliderTransportation);
+      transportationCost += costs.transportation.option1 * transportationMultipliers.option1;
+      transportationCost += costs.transportation.option2 * transportationMultipliers.option2;
+      transportationCost += costs.transportation.option3 * transportationMultipliers.option3;
+
+      $scope.cost.amount = foodCost + transportationCost + accommodationCost;
     };
   });
