70c94498cdec33db7e7ddf16efc7d5ab91ca5dd8
Caio Kinzel Filho <caiokf@gmail.com>
2015-10-05 12:05:59 +0200

    service tests

--- public/js/services/costs.js
+++ public/js/services/costs.js
@@ -1,5 +1,5 @@
 angular.module('travelCosts.services', [])
-  .factory('CostsService', ['$http', function ($http) {
+  .factory('CostsService', function () {
     return {
       getCostFor: function (location) {
         return { name: 'Brazil', food: {option1: 3.31, option2: 14.22, option3: 44.41 }, transportation: {option1: 0, option2: 16.2, option3: 24.3 }, accommodation: {option1: 0, option2: 7.60575, option3: 20.0876666666667 }};
@@ -20,4 +20,4 @@ angular.module('travelCosts.services', [])
       }
     };
 
-}]);
+});
---
+++ specs/public/services/costService.spec.js
@@ -0,0 +1,34 @@
+describe('Costs Service', function() {
+
+  var CostsService;
+
+  beforeEach(module('travelCosts.services'));
+
+  beforeEach(inject(function (_CostsService_) {
+    CostsService = _CostsService_;
+  }));
+
+  describe('get cost for', function () {
+    it('should return json', function () {
+      var result = CostsService.getCostFor('');
+      expect(result).to.be.an('object');
+    });
+  });
+
+  describe('get factored costs', function () {
+    it('should return costs according to slider', function () {
+      var costs = { 
+        food: {option1: 1, option2: 2, option3: 3 }, 
+        transportation: {option1: 1, option2: 2, option3: 3 }, 
+        accommodation: {option1: 1, option2: 2, option3: 3 }
+      };
+
+      var result = CostsService.getFactoredCosts(costs, 0, 0, 0);
+
+      expect(result.food).to.equal(1);
+      expect(result.transportation).to.equal(1);
+      expect(result.accommodation).to.equal(1);
+      expect(result.total).to.equal(3);
+    });
+  });
+});
